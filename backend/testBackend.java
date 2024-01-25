cab096bf08d7416804f048bcb75afe5604c874b0
mohazem-cell <mohamed20031253.com>
2024-01-25 02:02:07 +0200

    test

--- backend/testBackend.java
+++ backend/testBackend.java
@@ -2,18 +2,6 @@ package backend;
 
 public class testBackend {
     public static void main(String[] args) {
-        // GameEngin g = new GameEngin();
-        // ObserverGame o=new ObserverGame();
-        // while (!g.gameOver()) {
-        //     try (Scanner s = new Scanner(System.in)) {
-        //         // System.out.println("efefef");
-        //         int row = s.nextInt();
-        //         int col = s.nextInt();
-        //         GamePostion pos = new GamePostion(row, col);
-        //         g.setPlace(pos);
-        //     }
-        //     o.registerObserver(g);
-        // }
         TwoPlayers twoPlayers = new TwoPlayers();
     }
 }
