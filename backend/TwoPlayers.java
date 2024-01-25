69d5b87c1c581a452280cb19519359934c430e39
mohazem-cell <mohamed20031253.com>
2024-01-25 05:06:39 +0200

    New Branch after Modification

--- backend/TwoPlayers.java
+++ backend/TwoPlayers.java
@@ -1,19 +1,21 @@
 package backend;
 
-import java.util.Scanner;
-
 public class TwoPlayers {
-    GameEngin g = new GameEngin();
-    ObserverGame o = new ObserverGame();
-    public TwoPlayers() {
-        while (!g.gameOver()) {
-            Scanner s = new Scanner(System.in);
-            // System.out.println("efefef");
-            int row = s.nextInt();
-            int col = s.nextInt();
-            GamePostion pos = new GamePostion(row, col);
-            g.setPlace(pos);
-            o.registerObserver(g);
-        }
+    GameEngin g;
+    ObserverGame o;
+    private int row;
+    private int col;
+
+    public TwoPlayers(GameEngin g, ObserverGame o) {
+        this.g = g;
+        this.o = o;
+    }
+
+    public void play(int r, int c) {
+        row = r;
+        col = c;
+        GamePostion pos = new GamePostion(row, col);
+        g.setPlace(pos);
+        o.registerObserver(g);
     }
 }
--- backend/testBackend.java
+++ backend/testBackend.java
@@ -1,9 +1,39 @@
 package backend;
 
+import java.util.Scanner;
+
 public class testBackend {
     public static void main(String[] args) {
-        
-        
-        TwoPlayers twoPlayers = new TwoPlayers();
+        GameEngin g = new GameEngin();
+        ObserverGame o = new ObserverGame();
+        // GameEngin g = new GameEngin();  
+        // ObserverGame o=new ObserverGame();
+        // while (!g.gameOver()) {
+        // try (Scanner s = new Scanner(System.in)) {
+        // // System.out.println("efefef");
+        // GamePostion pos = new GamePostion(row, col);
+        // g.setPlace(pos);
+        // }
+        // o.registerObserver(g);
+        // }
+        // TwoPlayers twoPlayers = new TwoPlayers(g, o);
+        // for(int i=0;i<9;i++){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the index: ");
+        // int row = sc.nextInt();
+        // int col = sc.nextInt();
+        // twoPlayers.play(row,col);
+        // }
+        OnePlayer onePlayers = new OnePlayer(g, o);
+        for (int i = 0; i < 9; i++) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the index: ");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            onePlayers.play(row, col);
+            if (g.gameOver())
+                break;
+        }
+        // twoPlayers.play(i);
     }
 }
